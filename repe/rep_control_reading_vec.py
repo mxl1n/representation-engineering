8e12852eef4ae685f8ab00541831b8d5661a0015
ML <70348681+mxl1n@users.noreply.github.com>
2023-12-27 20:33:38 -0500

    random delete

--- repe/rep_control_reading_vec.py
+++ repe/rep_control_reading_vec.py
@@ -142,14 +142,14 @@ class WrappedReadingVecModel(torch.nn.Module):
 
     def wrap(self, layer_id, block_name):
         assert block_name in BLOCK_NAMES
-        if self.is_wrapped(self.model.model.layers[layer_id]):
-            block = getattr(self.model.model.layers[layer_id].block, block_name)
-            if not self.is_wrapped(block):
-                setattr(self.model.model.layers[layer_id].block, block_name, WrappedBlock(block))
-        else:
-            block = getattr(self.model.model.layers[layer_id], block_name)
-            if not self.is_wrapped(block):
-                setattr(self.model.model.layers[layer_id], block_name, WrappedBlock(block))
+        # if self.is_wrapped(self.model.model.layers[layer_id]):
+        #     block = getattr(self.model.model.layers[layer_id].block, block_name)
+        #     if not self.is_wrapped(block):
+        #         setattr(self.model.model.layers[layer_id].block, block_name, WrappedBlock(block))
+        # else:
+        #     block = getattr(self.model.model.layers[layer_id], block_name)
+        #     if not self.is_wrapped(block):
+        #         setattr(self.model.model.layers[layer_id], block_name, WrappedBlock(block))
 
     def wrap_decoder_block(self, layer_id):
         block = self.model.model.layers[layer_id]
@@ -267,8 +267,8 @@ class WrappedReadingVecModel(torch.nn.Module):
         for l, layer in enumerate(self.model.model.layers):
             if self.is_wrapped(layer):
                 self.model.model.layers[l] = layer.block
-            # for block_name in BLOCK_NAMES:
-            #     if self.is_wrapped(getattr(self.model.model.layers[l], block_name)):
-            #         setattr(self.model.model.layers[l],
-            #                 block_name,
-            #                 getattr(self.model.model.layers[l], block_name).block)
+            for block_name in BLOCK_NAMES:
+                if self.is_wrapped(getattr(self.model.model.layers[l], block_name)):
+                    setattr(self.model.model.layers[l],
+                            block_name,
+                            getattr(self.model.model.layers[l], block_name).block)
